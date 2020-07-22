4a280fc1a3ba30a79fb5260fc7ef08b18cf36542
Lena Vu <lenavu@Lenas-MacBook-Pro.local>
2020-07-22 20:12:35 +0300

    final

--- server.js
+++ server.js
@@ -6,19 +6,20 @@ const cors= require('cors')
 var request=require('request')
 const app = express();
 app.use(cors())
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/news'));
-app.get('/news',(req,res)=>{
-  request('https://newsapi.org/v2/top-headlines?' +
-  'country=us&' +
-  'apiKey=b0dd17deb9b34f2abac8026a821a2b3b',function(error,response,body){
-      if(!error && response.statusCode==200){
-          res.send(body)
-      } else {
-          console.log(error);
-      }
-  })
-})
+// app.get('/news',(req,res)=>{
+//   request('https://newsapi.org/v2/top-headlines?' +
+//   'country=us&' +
+//   'apiKey=b0dd17deb9b34f2abac8026a821a2b3b',function(error,response,body){
+//       if(!error && response.statusCode==200){
+//           res.send(body)
+//       } else {
+//           console.log(error);
+//       }
+//   })
+// })
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/news/index.html'));
